b1652cafd49747e6e1587935c945ea8ea3db318c
laineantti <laineantti@users.noreply.github.com>
2020-10-28 15:50:45 +0200

    Tehtävä 2.1 valmis!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [paivat, setPaivat] = useState(["ma", "ti", "ke", "to", "pe", "la", "su"])
+  const [tyotunnit, setTyotunnit] = useState([0, 0, 0, 0, 0, 0, 0])
+
+  const tunnitMuuttuneet = (event, i) => {
+    var tyotunnitCopy = [...tyotunnit]
+    tyotunnitCopy[i] = event.target.value
+    setTyotunnit(tyotunnitCopy)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>JavaScript-ohjelmointi II, (taulukot)</h1><hr />
+      <ul>
+        {paivat.map((paiva, i) => (
+          <li>{paiva} ({tyotunnit[i]} h)<input
+          onChange={(event) => tunnitMuuttuneet(event, i)}
+        ></input></li>
+        )
+        )}
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
