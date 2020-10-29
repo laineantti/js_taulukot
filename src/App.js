7921ff6923895477be04cf1999622d064cd1c152
laineantti <laineantti@users.noreply.github.com>
2020-10-29 22:30:20 +0200

    :D

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 function App() {
-  const [paivat, setPaivat] = useState(["ma", "ti", "ke", "to", "pe", "la", "su"])
-  const [tyotunnit, setTyotunnit] = useState([0, 0, 0, 0, 0, 0, 0])
-
-  const tunnitMuuttuneet = (event, i) => {
-    var tyotunnitCopy = [...tyotunnit]
-    tyotunnitCopy[i] = event.target.value
-    setTyotunnit(tyotunnitCopy)
-  }
+  const data = [{ "ma": 44 }, { "pe": 100 }, { "ke": 21 }, { "ti": 66 }, { "la": 22 }]
+  const day = ["ma", "ti", "ke", "to", "pe", "la", "su"]
 
   return (
     <div>
-      <h1>JavaScript-ohjelmointi II, (taulukot)</h1><hr />
+      <h1>JavaScript-ohjelmointi II, (taulukot)</h1>
+
+      <hr /><h2>Tehtävä 2.8</h2>
+      <p>Sort järjestää taulukon kappaleet ja palauttaa järjestetyn taulukon. Compare on
+        funktio joka määrittää sen millä tavalla taulukko järjestetään uudelleen.</p>
+
+      <hr /><h2>Tehtävä 2.9</h2>
       <ul>
-        {paivat.map((paiva, i) => (
-          <li>{paiva} ({tyotunnit[i]} h)<input
-          onChange={(event) => tunnitMuuttuneet(event, i)}
-        ></input></li>
-        )
-        )}
+        {[...data].sort((a, b) => Object.values(a) - Object.values(b))
+          .map(p => <li><pre>{Object.keys(p)}  {Object.values(p)}</pre></li>)}
       </ul>
+
+      <hr /><h2>Tehtävä 2.10</h2>
+      <ul>
+        {[...data].sort((a, b) => {
+          let pA = day.indexOf(Object.keys(a)[0]), pB = day.indexOf(Object.keys(b)[0])
+          return (pA < pB ? -1 : pA > pB ? 1 : 0)
+        }).map(p => <li><pre>{Object.keys(p)} {Object.values(p)}</pre></li>)}
+      </ul>
+
+      <hr /><h2>Tehtävä 2.11</h2>
+      <ul>
+        {[...data].map(p => Object.values(p) % 2 === 0 ? <li><pre>{Object.keys(p)}  {Object.values(p)}</pre></li> : null)}
+      </ul>
+
+      <hr /><h2>Tehtävä 2.12</h2>
+      <ul>
+        {[...data].map(p => Object.keys(p).toString().split('')[1] === "e" ? <li><pre>{Object.keys(p)}  {Object.values(p)}</pre></li> : null)}
+      </ul>
+
     </div>
   )
 }
